52be5e4eba85beae92ed88beac1217a5983cf87e
wuyeelok <wuyeelok@gmail.com>
2022-06-18 22:57:56 +0800

    Nav: Hide ham after clicking a link

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const mediaMax = 43 * 16; // same as max-width in style.css
+
 // Responsive nav by Ash Neilson
 $(function () {
   menu = $("nav ul");
@@ -8,23 +10,38 @@ $(function () {
   });
 
   $(window).resize(function () {
-    var w = $(this).width();
-    if (w > 480 && menu.is(":hidden")) {
+    let w = $(this).width();
+    if (w > mediaMax && menu.is(":hidden")) {
       menu.removeAttr("style");
     }
   });
 
-  $("nav li").on("click", function (e) {
-    var w = $(window).width();
-    if (w < 480) {
-      menu.slideToggle();
-    }
-  });
   $(".open-menu").height($(window).height());
 });
 
 // Smooth Scrolling
-$(".cf a, .internal-link").on("click", function (event) {
+$(".cf a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      { scrollTop: $(hash).offset().top },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+
+    const w = $(window).width();
+    if (w <= mediaMax) {
+      menu.slideToggle();
+    }
+  }
+});
+
+$(".internal-link").on("click", function (event) {
   if (this.hash !== "") {
     event.preventDefault();
 
