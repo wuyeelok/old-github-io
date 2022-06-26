cf805cbdf76625eb66d641878b65ca2726e65586
wuyeelok <wuyeelok@gmail.com>
2022-06-26 19:11:10 +0800

    Useful function to detect deviceType

--- js/main.js
+++ js/main.js
@@ -85,3 +85,18 @@ if (skill_icons !== null) {
     });
   });
 }
+
+// Refer to https://attacomsian.com/blog/javascript-detect-mobile-device
+const deviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  } else if (
+    /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
